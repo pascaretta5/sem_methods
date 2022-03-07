216b1fdcac09a72080ce0430a7ab9d124a08514a
sarahbalboa <40496531@live.napier.ac.uk>
2022-03-07 13:07:35 +0000

    Issue #1 Fully working

--- src/main/java/com/napier/sem_group22/App.java
+++ src/main/java/com/napier/sem_group22/App.java
@@ -1,6 +1,7 @@
 package com.napier.sem_group22;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 
 public class App
@@ -34,7 +35,7 @@ public class App
             {
                 // Wait a bit for db to start
                 Thread.sleep(30000);
-                // Connect to database
+                // Connect to database   ---- localhost:33060 -- db:3306
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
                 break;
@@ -69,6 +70,65 @@ public class App
             }
         }
     }
+    /* ----------- getCountryPopulationLargeToSmall() -- Issue #1 ---------------
+    Objective: get the name and the population from the workd database in order of lagest to smallest population.
+    Parameters: None
+    Return Type: ArrayList<Country> -- returns an aray with all the contries
+    */
+    public ArrayList<Country> getCountryPopulationLargeToSmall() {
+
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+
+            // Create string for SQL statement
+            String strPopulationLageSmall =
+                    "SELECT Population, Name "
+                    +"FROM country "
+                    + "ORDER BY Population DESC;";
+
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strPopulationLageSmall);
+
+            //Create Country ArrayList
+            ArrayList<Country> countries = new ArrayList<Country>();
+
+            // Check one is returned
+            while (rset.next())
+            {
+                Country count = new Country();
+                count.name = rset.getString("Name");
+                count.population = rset.getInt("Population");
+                countries.add(count);
+            }
+            return countries;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get countries population");
+            return null;
+        }
+    }
+
+    /* ----------- printCountriesPop(ArrayList<Country> countries) ---------------
+    Objective: print columns names and all the data from the countries(name, population) in the ArrayList.
+    Parameters: ArrayList<Country> countries -- takes an array with all the cuontries.
+    Return type: VOID
+    */
+    public void printCountriesPop(ArrayList<Country> countries)
+    {
+        // Print header
+        System.out.println(String.format("%-10s %-25s", "Population", "Name"));
+        // Loop over all employees in the list
+        for (Country c : countries)
+        {
+            String c_string =
+                    String.format("%-10s %-25s", c.population, c.name);
+            System.out.println(c_string);
+        }
+    }
     public static void main(String[] args)
     {
         // Create new Application
@@ -76,6 +136,12 @@ public class App
 
         // Connect to database
         a.connect();
+
+        // ---------- Issue #1 ----------
+        //call method to execute the query and get the population
+        ArrayList<Country> countries = a.getCountryPopulationLargeToSmall();
+        //print Name and Population in console
+        a.printCountriesPop(countries);
 
         // Disconnect from database
         a.disconnect();
---
+++ src/main/java/com/napier/sem_group22/Country.java
@@ -0,0 +1,29 @@
+package com.napier.sem_group22;
+
+public class Country {
+    public String code;
+
+    public String name;
+
+    public String continent;
+
+    public String region;
+
+    public double surface_area;
+
+    public int population;
+
+    public double life_expectancy;
+
+    public double gnp;
+
+    public double gnp_old;
+
+    public String local_name;
+
+    public String government_form;
+
+    public String head_of_state;
+
+    public int capital;
+}
