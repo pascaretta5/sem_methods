be26948d8c53c76f1925f4bbdb8a753ef87dc736
sarahbalboa <40496531@live.napier.ac.uk>
2022-03-24 12:44:35 +0000

    Integration Test separately successful

--- src/test/java/com/napier/sem_group22/Test_Integration.java
+++ src/test/java/com/napier/sem_group22/Test_Integration.java
@@ -21,34 +21,4 @@ public class Test_Integration {
     }
 
 
-
-    @Test
-    void getNTopPopCountriesRegionTest()
-    {
-        ArrayList<Country> countries1 = issue6.getNTopPopCountriesRegion("1", app, "Central America");
-        ArrayList<Country> countries2 = new ArrayList<>();
-        Country c1 = new Country();
-        c1.code = "MEX";
-        c1.name = "Mexico";
-        c1.continent = "North America";
-        c1.region = "Central America";
-        c1.population = 98881000;
-        c1.capitalName = "Ciudad de México";
-
-        countries2.add(c1);
-
-        for(int i = 0; i < countries1.size(); i++ ) {
-            assertEquals(countries2.get(i).code, countries1.get(i).code);
-            assertEquals(countries2.get(i).name, countries1.get(i).name);
-            assertEquals(countries2.get(i).continent, countries1.get(i).continent);
-            assertEquals(countries2.get(i).region, countries1.get(i).region);
-            assertEquals(countries2.get(i).population, countries1.get(i).population);
-            assertEquals(countries2.get(i).capitalName, countries1.get(i).capitalName);
-        }
-    }
-
-
-
-
-
 }
--- src/test/java/com/napier/sem_group22/Test_IntegrationIssue6.java
+++ src/test/java/com/napier/sem_group22/Test_IntegrationIssue6.java
@@ -1,3 +1,8 @@
+/**
+ * Author: Sara
+ * Date: 22/02/22
+ * Issue 6 tests
+ */
 package com.napier.sem_group22;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -8,7 +13,6 @@ import java.util.ArrayList;
 
 public class Test_IntegrationIssue6 {
     static App app;
-
     static Issue6 issue6;
 
     @BeforeAll
@@ -17,6 +21,32 @@ public class Test_IntegrationIssue6 {
         app = new App();
         issue6 = new Issue6();
         app.connect("localhost:33060", 30000);
+    }
+
+
+    @Test
+    void getNTopPopCountriesRegionTest()
+    {
+        ArrayList<Country> countries1 = issue6.getNTopPopCountriesRegion("1", app, "Central America");
+        ArrayList<Country> countries2 = new ArrayList<>();
+        Country c1 = new Country();
+        c1.code = "MEX";
+        c1.name = "Mexico";
+        c1.continent = "North America";
+        c1.region = "Central America";
+        c1.population = 98881000;
+        c1.capitalName = "Ciudad de México";
+
+        countries2.add(c1);
+
+        for(int i = 0; i < countries1.size(); i++ ) {
+            assertEquals(countries2.get(i).code, countries1.get(i).code);
+            assertEquals(countries2.get(i).name, countries1.get(i).name);
+            assertEquals(countries2.get(i).continent, countries1.get(i).continent);
+            assertEquals(countries2.get(i).region, countries1.get(i).region);
+            assertEquals(countries2.get(i).population, countries1.get(i).population);
+            assertEquals(countries2.get(i).capitalName, countries1.get(i).capitalName);
+        }
     }
 
     @Test
