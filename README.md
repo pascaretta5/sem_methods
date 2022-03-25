6307849f9bca1a13c9ecfbde3b3ac80e96da5eee
40511028 <40511028@live.napier.ac.uk>
2022-03-25 14:29:17 +0000

    Issue29

--- README.md
+++ README.md
@@ -10,7 +10,6 @@ Bruno Pascaretta Guerra - 40536560@live.napier.ac.uk
 Sara Hussein Celda - 40496531@live.napier.ac.uk
 
 
-**Backlog:** https://trello.com/b/OAWpc20Q/backlog
 
 ![workflow](https://github.com/40511028/SEM_GROUP22/actions/workflows/main.yml/badge.svg)
 [![Releases](https://img.shields.io/github/release/40511028/SEM_GROUP22/all.svg?style=flat-square)](https://github.com/40511028/SEM_GROUP22/releases)
---
+++ src/main/java/com/napier/sem_group22/Issue5.java
@@ -0,0 +1,59 @@
+package com.napier.sem_group22;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class Issue5 {
+    /**
+     * --------------------Issue #5: All the top N countries in a continent where N is provided by the user--------
+     *    Objective: get all N top populated countries in continents -- Large to Small.
+     *    @param continent
+     *    @param limit
+     *    @return ArrayList<Country>
+     */
+    public ArrayList<Country> getTopCountryByContinentLargeToSmall(App app, String continent, Integer limit) {
+
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = app.con.createStatement();
+
+            // Create string for SQL statement
+
+            //Needed to join both table CITY and COUNTRY in order to retrieve the right capital.
+            String strPopulationLageSmall =
+                    "SELECT country.Code, country.Name, country.Continent, country.Region, country.Population, city.Name "
+                            + "FROM city JOIN country ON CountryCode = Code "
+                            + "WHERE country.capital = city.ID AND Continent LIKE '" + continent +"' "
+                            + "ORDER BY Population DESC "
+                            + "LIMIT " + limit + "; ";
+
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strPopulationLageSmall);
+
+            //Create Country ArrayList
+            ArrayList<Country> countries = new ArrayList<Country>();
+
+            // Check one is returned and go through all countries and to get the details
+            while (rset.next())
+            {
+                Country count = new Country();
+                count.code = rset.getString("country.Code");
+                count.region = rset.getString("country.Region");
+                count.capitalName = rset.getString("city.Name");
+                count.continent = rset.getString("country.Continent");
+                count.name = rset.getString("country.Name");
+                count.population = rset.getInt("country.Population");
+                countries.add(count); // add country in ArrayList<Country> countries
+            }
+            return countries; // return ArrayList
+        }
+        catch (Exception e) //no country found
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get countries population");
+            return null;
+        }
+    }
+}
